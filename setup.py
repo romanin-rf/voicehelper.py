4c452ac189c5e3cd5ff141712d0c319e4cc28d7b
Romanin <60302782+romanin-rf@users.noreply.github.com>
2022-05-19 11:44:48 +0200

    Release v0.1.2.1
    - Дописан setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 import os
 
+# Функция получения полных путей к папкам и подпапкам
 def globalizer(dirpath: str) -> list:
 		files = []
 		folder_abspath = os.path.abspath(dirpath)
@@ -16,9 +17,23 @@ def globalizer(dirpath: str) -> list:
 			files.append(folder_abspath)
 		return files
 
+# Получение информации о установке
+try:
+    from wheel.bdist_wheel import bdist_wheel
+except ImportError:
+    cmdclass = {}
+else:
+    class bdist_wheel_tag_name(bdist_wheel):
+        def get_tag(self):
+            abi = 'none'
+            # TODO: Надо дописать проверку системы
+            return 'py39', abi, 'win_amd64'
+    cmdclass = {'bdist_wheel': bdist_wheel_tag_name}
+
+# Установка
 setuptools.setup(
     name="voicehelper.py",
-    version="0.1.2",
+    version="0.1.2.1",
     description="These are two neural networks connected by the same module. One for recognition, the other for voice generation.",
     long_description=open("README.md", "r", errors="ignore").read(),
     long_description_content_type="text/markdown",
@@ -27,8 +42,9 @@ setuptools.setup(
     author_email="semina054@gmail.com",
     license="MIT",
     packages=setuptools.find_packages(),
-    package_data={"VoiceHelper": globalizer("VoiceHelper")},
+    package_data={"VoiceHelper": globalizer("VoiceHelper" + os.sep + "models")},
     include_package_data=True,
+    cmdclass=cmdclass,
     python_requires=">=3.9",
     install_requires=["numpy", "sounddevice", "torch", "vosk", "silero", "vbml", "rich"],
     setup_requires=["numpy", "sounddevice", "torch", "vosk", "silero", "vbml", "rich"]
